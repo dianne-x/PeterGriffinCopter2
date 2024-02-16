37cef4a0b19a639473d0a00ea60c44c1218044d0
Bobák Kornél <143836544+dianne-x@users.noreply.github.com>
2024-02-16 13:52:46 +0100

    Add project files.

---
+++ PeterGriffinCopter2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace PeterGriffinCopter2
+{
+    class chainOne
+    {
+        public int data;
+        public chainOne next = null;
+        
+
+        public chainOne(int value)
+        {
+            data = value;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            chainOne head  = null;
+
+            chainOne node1 = new chainOne(1);
+            head = node1;
+
+            chainOne node2 = new chainOne(2);
+            node1.next = node2;
+
+            chainOne node3 = new chainOne(3);
+            node2.next = node3;
+
+            chainOne item = head;
+            while (item != null)
+            {
+                System.Console.WriteLine(item.data);
+                item = item.next;
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
